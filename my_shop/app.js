55d5b2396f9ee40ef0f705c67319f0e722e89ce7
Aman Gutpa <2015aman.a@ves.ac.in>
2020-09-10 18:45:07 +0530

    changing userID

--- my_shop/app.js
+++ my_shop/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findById("5f58c9250b86e9035c22bb23")
+    User.findById("5f59da0eeddbb91a94e95138")
         .then(user => {
             req.user = user
             next()
