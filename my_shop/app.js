4da53849dc4d785598c63d4ad8eb5231bc0aee99
Aman Gutpa <2015aman.a@ves.ac.in>
2020-09-17 10:28:56 +0530

    adding express-session as middleware and registering mongodb as session store

--- my_shop/app.js
+++ my_shop/app.js
@@ -2,12 +2,20 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const mongoose = require("mongoose")
 const User = require('./models/user');
 
+const MONGODB_CON_STRING = "mongodb+srv://aman:snZ5L0a4JMbsXqWG@primary.u62r1.mongodb.net/shop"
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_CON_STRING,
+    collection: "sessionStore"
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,17 +26,8 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: "ShouldBeABigString", resave: false, saveUninitialized: false, store: store}));
 
-app.use((req, res, next) => {
-    User.findById("5f59da0eeddbb91a94e95138")
-        .then(user => {
-            req.user = user
-            next()
-        })
-        .catch(err => {
-            console.log('Adding user to req object', err)
-        })
-})  
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -37,7 +36,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
     
 mongoose
-    .connect("mongodb+srv://aman:snZ5L0a4JMbsXqWG@primary.u62r1.mongodb.net/shop?retryWrites=true&w=majority", {useUnifiedTopology: true, useNewUrlParser: true})
+    .connect(MONGODB_CON_STRING, {useUnifiedTopology: true, useNewUrlParser: true})
     .then(result => {
         console.log("Connected")
         return User.findOne()
