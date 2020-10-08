43294a55f595bf8850d42105538e135d7e32a72f
Aman Gutpa <2015aman.a@ves.ac.in>
2020-10-08 22:59:24 +0530

    changing route files to add expess-validators and sanitizers

--- my_shop/routes/admin.js
+++ my_shop/routes/admin.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const express = require('express');
+const { check, body } = require('express-validator/check')
 
 const adminController = require('../controllers/admin');
 const authMiddleware = require("../middleware/is-auth")
@@ -17,10 +18,60 @@ router.get('/products', authMiddleware, adminController.getProducts);
 router.get('/edit-product/:productId', authMiddleware, adminController.getEditProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', authMiddleware, adminController.postAddProduct);
+router.post('/add-product',     
+    [
+        body("title")
+            .isString()
+            .isLength({
+                min: 3
+            })
+            .trim(),
+        
+        body("imageUrl")
+            .isURL(),
+        
+        body("price")
+            .isFloat(),
+
+        body("description")
+            .isLength({
+                min: 3,
+                max: 400
+            })
+            .trim()
+        
+    ],
+    authMiddleware,
+    adminController.postAddProduct
+);
 
 // /admin/edit-product
-router.post('/edit-product', authMiddleware, adminController.postEditProduct);
+router.post('/edit-product',
+    [
+        body("title")
+            .isString()
+            .isLength({
+                min: 3
+            })
+            .trim(),
+        
+        body("imageUrl")
+            .isURL(),
+        
+        body("price")
+            .isFloat(),
+
+        body("description")
+            .isLength({
+                min: 3,
+                max: 400
+            })
+            .trim()
+    
+    ],
+    authMiddleware,
+    adminController.postEditProduct
+);
 
 // /admin/delete-product
 router.post('/delete-product', authMiddleware, adminController.postDeleteProduct);
--- my_shop/routes/auth.js
+++ my_shop/routes/auth.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { check, body } = require('express-validator/check')
 
 const authController = require('../controllers/auth');
 
 const router = express.Router();
+const User = require('../models/user');
 
 
 router.get("/login", authController.getLogin)
@@ -17,10 +19,64 @@ router.post("/new-password", authController.postNewPwd)
 
 router.post("/reset", authController.postResetPwd)
 
-router.post("/login", authController.postLogin)
+router.post("/login", 
+    [
+        check("email")
+            .isEmail()
+            .withMessage("Please enter a valid email")
+            .normalizeEmail(),
+
+        body("password", "Please enter a password with only number and alphabets and should be atleast 6 characters long." ) // use the second argument if you want only one meesage to return for all types of error encountered
+            .isLength({
+                min: 6
+            })
+            .isAlphanumeric()
+            .trim(),
+    ], 
+    authController.postLogin)
 
 router.post("/logout", authController.postLogout)
 
-router.post("/signup", authController.postSignup)
+router.post(
+    "/signup", 
+    [
+        check("email")
+            .isEmail()
+            .withMessage("Please enter a valid email")
+            .normalizeEmail()
+            .custom((value, {req}) => {
+            //     if (value !== someCustomValidation){
+            //         throw new Error("custom message")
+            //         or
+            //         return false //to show default message
+            //     }
+            //     return true // if all went well
+                return User.findOne({email: value})
+                    .then((user) => {
+                        if (user){
+                            return Promise.reject("Email ID already exists. Please use a different one")
+                        }
+                    })
+            }),
+
+        // The second argument bcomes the default message for every type of validation that you peform on a field so avoid using withMessage() everytime.
+        body("password", "Please enter a password with only number and alphabets and should be atleast 6 characters long.")
+            .isLength({
+                min: 6
+            })
+            .isAlphanumeric()
+            .trim(),
+
+        body("confirmPassword")
+            .trim()
+            .custom((value, { req }) => {
+                if (value !== req.body.password) {
+                    throw new Error("Password Mismatch")
+                }
+                return true
+            })
+    ],
+    authController.postSignup
+)
 
 module.exports = router;
